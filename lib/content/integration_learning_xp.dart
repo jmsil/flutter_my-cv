283f23848b5430e3ba0a9884eb28d3f45de625d0
jmsil <jmsilva.inbox@gmail.com>
2026-04-13 13:58:37 -0300

    Create the ProjectTextWidget widget for font style standardization.

--- lib/content/integration_learning_xp.dart
+++ lib/content/integration_learning_xp.dart
@@ -6,6 +6,7 @@ import '../ui/theme.dart';
 import '../ui/viewer/project.dart';
 import '../ui/viewer/project_code.dart';
 import '../ui/viewer/project_image.dart';
+import '../ui/viewer/text.dart';
 import 'expandable_info.dart';
 
 class IntegrationLearningExperience extends ExpandableInfo {
@@ -37,10 +38,7 @@ class _Calculator extends Project {
   List<Widget> buildViewer() {
     return [
       ProjectImageWidget(assets.getFile(1), false, true),
-      Text(
-        StringsProvider.strings.integrationProjectCalculatorInfo,
-        style: AppTheme.darkStyle
-      ),
+      Text(StringsProvider.strings.integrationProjectCalculatorInfo),
       ProjectImageWidget(assets.getFile(2), true, true)
     ];
   }
@@ -59,16 +57,10 @@ class _ConversionsAndFtp extends Project {
   List<Widget> buildViewer() {
     return [
       ProjectImageWidget(assets.getFile(1), false, true),
-      Text(
-        StringsProvider.strings.integrationProjectConversionsAndFtpInfo1,
-        style: AppTheme.darkStyle
-      ),
+      ProjectTextWidget(StringsProvider.strings.integrationProjectConversionsAndFtpInfo1),
       ProjectImageWidget(assets.getFile(2), true, false),
       ProjectCodeWidget(assets.getFile(3), assets.getFile(4), true, true),
-      Text(
-        StringsProvider.strings.integrationProjectConversionsAndFtpInfo2,
-        style: AppTheme.darkStyle
-      ),
+      ProjectTextWidget(StringsProvider.strings.integrationProjectConversionsAndFtpInfo2),
       ProjectImageWidget(assets.getFile(5), true, false),
       ProjectCodeWidget(assets.getFile(6), assets.getFile(7), true, true)
     ];
---
+++ lib/ui/viewer/text.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import '../theme.dart';
+
+class ProjectTextWidget extends Text {
+  ProjectTextWidget(String text)
+    : super(text, style: AppTheme.darkStyle);
+}
